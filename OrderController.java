016de532d46c469d6c49ec2a4bb78ebd7068bf9e
lauden <Lcedwards21@coe.edu>
2023-04-14 13:34:04 -0500

    new Quick edit

--- OrderController.java
+++ OrderController.java
@@ -1,2 +1,16 @@
 public class OrderController {
+
+    OrderModel orderModel;
+    OrderView orderView;
+
+    public OrderController(OrderModel orderModel, OrderView orderView) {
+        this.orderModel = orderModel;
+        this.orderView = orderView;
+    }
+
+    public void additem(OrderItem orderItem) {
+
+    }
+
+    //public void clear
 }
